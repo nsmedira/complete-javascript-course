5b876d03d0fbf5d01170427fec6a9a58bb5b2d02
Nick Smedira <nsmedira@me.com>
2020-10-11 06:58:00 -0400

    practice IIFE, closures, public vs private

---
+++ budget-app/app.js
@@ -0,0 +1,34 @@
+// controller module will be an immediately invoked function expression that returns an object
+var budgetController = (function() {
+
+    var x = 23
+
+    var add = function(a) {
+        return x + a
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b)
+        }
+    }
+
+})()
+
+var UIController = (function() {
+
+    // some code
+
+})()
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5)
+
+    return {
+        anotherPublic: function() {
+            console.log(z)
+        }
+    }
+
+})(budgetController, UIController)
